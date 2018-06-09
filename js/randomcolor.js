7074938e0380fc5151f360006a2bfcbea235b7e1
Sara Peeters <sara.peeters@gmail.com>
2018-06-09 12:14:05 +0200

    minimal working version

---
+++ js/randomcolor.js
@@ -0,0 +1,14 @@
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const randomColor = (() => {
+  "use strict";
+
+  return () => {
+    var h = randomInt(0, 360);
+    var s = randomInt(42, 98);
+    var l = randomInt(40, 90);
+    return `hsl(${h},${s}%,${l}%)`;
+  };
+})();
